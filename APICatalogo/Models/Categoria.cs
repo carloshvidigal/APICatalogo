aa14b4c9636cee24f3b4fab63d5443376f71df5d
carloshvidigal <chmovi@hotmail.com>
2025-03-27 14:09:45 -0300

    Criação dos models

---
+++ APICatalogo/Models/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+
+namespace APICatalogo.Models
+{
+    public class Categoria
+    {
+        public Categoria() 
+        { 
+            Produtos = new Collection<Produto>();
+        }
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string? ImagemURL { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public ICollection<Produto> Produtos { get; set; } 
+
+    }
+}
---
+++ APICatalogo/Models/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace APICatalogo.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? ImagemURL { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+
+    }
+}
