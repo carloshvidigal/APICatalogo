9ec7506c6ba17ef72188ba63c99fdd8fd858a9bb
carloshvidigal <chmovi@hotmail.com>
2025-03-27 15:47:26 -0300

    Inserção - Models - dos atributos Data Annotations

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,20 +1,28 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
         public Categoria() 
         { 
             Produtos = new Collection<Produto>();
         }
+
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(80)]
         public string? Nome { get; set; }
-        public string? Descricao { get; set; }
-        public decimal Preco { get; set; }
+
+        [Required]
+        [StringLength(300)]
         public string? ImagemURL { get; set; }
-        public float Estoque { get; set; }
-        public DateTime DataCadastro { get; set; }
+
         public ICollection<Produto> Produtos { get; set; } 
 
     }
