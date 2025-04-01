990ba172cb069cce3a3a2742a404a9915145b47d
carloshvidigal <chmovi@hotmail.com>
2025-04-01 19:29:12 -0300

    Implementa métodos PUT e DELETE na API

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -2,6 +2,7 @@
 using APICatalogo.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogo.Controllers
 {
@@ -51,5 +52,37 @@ namespace APICatalogo.Controllers
 
             return new CreatedAtRouteResult("ObterProduto", new { id = produto.Id }, produto);
         }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            if (id !=  produto.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+
+        [HttpDelete("{id:int}")]
+
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.Id==id);
+
+            if(produto is null)
+            {
+                return NotFound("Produto não localizado.");
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+
     }
 }
