cd87a5ee5b1160c2c7caf9e88a0c2e36f8cbb8a0
carloshvidigal <chmovi@hotmail.com>
2025-06-10 15:25:51 -0300

    Remover os blocos 'try' 'catch' substuindo por um filtro de exceção global

--- APICatalogo/Controllers/CategoriasController.cs
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -1,168 +1,85 @@
-﻿using System.Reflection.Metadata.Ecma335;
-using APICatalogo.Context;
+﻿using APICatalogo.Context;
 using APICatalogo.Filters;
 using APICatalogo.Models;
-using APICatalogo.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace APICatalogo.Controllers
+namespace APICatalogo.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class CategoriasController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CategoriasController : ControllerBase
+    private readonly AppDbContext _context;
+    private readonly ILogger<CategoriasController> _logger;
+
+    public CategoriasController(AppDbContext context, ILogger<CategoriasController> logger)
     {
-        private readonly AppDbContext _context;
-        private readonly IMeuServico _meuServico;
-        private readonly IConfiguration _configuration;
-        private readonly ILogger _logger;
+        _context = context;
+        _logger = logger;
+    }
 
-        public CategoriasController(AppDbContext context, IMeuServico meuServico, IConfiguration configuration, 
-            ILogger<CategoriasController> logger)
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Categoria>>> Get()
+    {
+        return await _context.Categorias.AsNoTracking().ToListAsync();
+    }
+
+    [HttpGet("{id:int}", Name = "ObterCategoria")]
+    public ActionResult<Categoria> Get(int id)
+    {
+        var categoria = _context.Categorias.FirstOrDefault(p => p.Id == id);
+
+        if (categoria == null)
         {
-            _context = context;
-            _meuServico = meuServico;
-            _configuration = configuration;
-            _logger = logger;
+            _logger.LogWarning($"Categoria com id= {id} não encontrada...");
+            return NotFound($"Categoria com id= {id} não encontrada...");
+        }
+        return Ok(categoria);
+    }
+
+    [HttpPost]
+    public ActionResult Post(Categoria categoria)
+    {
+        if (categoria is null)
+        {
+            _logger.LogWarning($"Dados inválidos...");
+            return BadRequest("Dados inválidos");
         }
 
-        // O sistema já é configurado para usar a anotação FromService como default
-        [HttpGet("UsandoFromServices/{nome}")]
-        public ActionResult<string> GetSaudacaoFromServices([FromServices] IMeuServico meuServico, string nome)
+        _context.Categorias.Add(categoria);
+        _context.SaveChanges();
+
+        return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.Id }, categoria);
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult Put(int id, Categoria categoria)
+    {
+        if (id != categoria.Id)
         {
-            return meuServico.Saudacao(nome);
+            _logger.LogWarning($"Dados inválidos...");
+            return BadRequest("Dados inválidos");
         }
 
-        [HttpGet("LerArquivoConfiguracao")]
-        public string GetValores()
+        _context.Entry(categoria).State = EntityState.Modified;
+        _context.SaveChanges();
+        return Ok(categoria);
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        var categoria = _context.Categorias.FirstOrDefault(p => p.Id == id);
+
+        if (categoria == null)
         {
-            var valor1 = _configuration["chave1"];
-            var valor2 = _configuration["chave2"];
-
-            var secao1 = _configuration["secao1:chave2"];
-
-            return $"Chave1 = {valor1} \nChave2 = {valor2} \nSeção1 => Chave2 = {secao1}";
+            _logger.LogWarning($"Categoria com id={id} não encontrada...");
+            return NotFound($"Categoria com id={id} não encontrada...");
         }
 
-        [HttpGet("produtos")]
-        public async Task<ActionResult<IEnumerable<Categoria>>> GetCategoriasProdutos()
-        {
-            _logger.LogInformation("==================== GET api/categorias/produtos ====================");
-            try
-            {
-                return await _context.Categorias.Include(p => p.Produtos).ToListAsync();
-            }
-            catch (Exception)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError,
-                    "Ocorreu um problema ao tratar a sua solicitação.");
-            }
-
-        }
-
-        [HttpGet]
-        [ServiceFilter(typeof(ApiLoggingFilter))]
-        public async Task<ActionResult<IEnumerable<Categoria>>> Get()
-        {
-            try
-            {
-                return await _context.Categorias.AsNoTracking().ToListAsync();
-            }
-            catch (Exception)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError,
-                    "Ocorreu um problema ao tratar a sua solicitação.");
-            }
-        }
-
-        [HttpGet("{id:int}", Name = "ObterCategoria")]
-        public ActionResult<Categoria> Get(int id)
-        {
-            try
-            {
-                var categoria = _context.Categorias.FirstOrDefault(p => p.Id == id);
-
-                _logger.LogInformation($"==================== GET api/categorias/id = {id} ====================");
-
-
-                if (categoria == null)
-                {
-                    _logger.LogInformation($"==================== GET api/categorias/id {id} NOT FOUND ====================");
-                    return NotFound($"Categoria com id= {id} não encontrada...");
-                }
-                return Ok(categoria);
-            }
-            catch (Exception)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError,
-                           "Ocorreu um problema ao tratar a sua solicitação.");
-            }
-        }
-
-        [HttpPost]
-        public ActionResult Post(Categoria categoria)
-        {
-            try
-            {
-                if (categoria is null)
-                    return BadRequest("Dados inválidos");
-
-                _context.Categorias.Add(categoria);
-                _context.SaveChanges();
-
-                return new CreatedAtRouteResult("ObterCategoria",
-                    new { id = categoria.Id }, categoria);
-
-            }
-            catch (Exception)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError,
-                          "Ocorreu um problema ao tratar a sua solicitação.");
-            }
-        }
-
-        [HttpPut("{id:int}")]
-        public ActionResult Put(int id, Categoria categoria)
-        {
-            try
-            {
-                if (id != categoria.Id)
-                {
-                    return BadRequest("Dados inválidos");
-                }
-                _context.Entry(categoria).State = EntityState.Modified;
-                _context.SaveChanges();
-                return Ok(categoria);
-            }
-            catch (Exception)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError,
-                       "Ocorreu um problema ao tratar a sua solicitação.");
-            }
-        }
-
-        [HttpDelete("{id:int}")]
-        public ActionResult<Categoria> Delete(int id)
-        {
-            try
-            {
-                var categoria = _context.Categorias.FirstOrDefault(p => p.Id == id);
-
-                if (categoria == null)
-                {
-                    return NotFound($"Categoria com id={id} não encontrada...");
-                }
-                _context.Categorias.Remove(categoria);
-                _context.SaveChanges();
-                return Ok(categoria);
-            }
-            catch (Exception)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError,
-                               "Ocorreu um problema ao tratar a sua solicitação.");
-            }
-        }
-
-
+        _context.Categorias.Remove(categoria);
+        _context.SaveChanges();
+        return Ok(categoria);
     }
 }
