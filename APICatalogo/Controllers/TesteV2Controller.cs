57f41ccc1a6a99a09e8350a9fee24dcea6b7fa4c
carloshvidigal <chmovi@hotmail.com>
2025-09-16 17:33:01 -0300

    Criar categorias de teste para versionamento

---
+++ APICatalogo/Controllers/TesteV2Controller.cs
@@ -0,0 +1,19 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers;
+
+    [Route("api/v{version:apiVersion}/teste")]
+    [ApiController]
+    [ApiVersion("2.0")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+
+public class TesteV2Controller : ControllerBase
+    {
+    [HttpGet]
+    public string GetVersion()
+        {
+            return "TesteV1 - GET - Api Versão 2.0";
+        }
+    }
+
